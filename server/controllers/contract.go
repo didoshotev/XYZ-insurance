47d31afe5db436f99b4e0bb73afef9471116d591
didoshotev <didoshotev@gmail.com>
2022-10-23 17:18:34 +0300

    feat: contract crud

--- server/controllers/contract.go
+++ server/controllers/contract.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"regexp"
+	"strconv"
 
 	"github.com/didoshotev/XYZ-insurance/models"
 )
@@ -12,24 +14,33 @@ type contractController struct {
 }
 
 func (cc contractController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/contract" {
+	if r.URL.Path == "/contracts" {
 		switch r.Method {
 		case http.MethodGet:
-			w.Write([]byte("get contract"))
+			cc.getContracts(w, r)
 		case http.MethodPost:
 			cc.createContractHandler(w, r)
-		case http.MethodPut:
-			w.Write([]byte("update contract"))
-		case http.MethodDelete:
-			w.Write([]byte("delete contract"))
 		default:
 			w.WriteHeader(http.StatusNotImplemented)
 		}
-		// } else if r.URL.Path == "/contracts" {
 	} else {
+		matches := cc.contractIDPattern.FindStringSubmatch(r.URL.Path)
+		if len(matches) == 0 {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		id, err := strconv.Atoi(matches[1])
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("cannot find user with this id"))
+		}
+
 		switch r.Method {
 		case http.MethodGet:
-			w.Write([]byte("Get all contracts"))
+			cc.getContract(id, w, r)
+		case http.MethodPut:
+			cc.editContractHandler(id, w, r)
+		case http.MethodDelete:
+			cc.deleteContractHandler(id, w, r)
 		default:
 			w.WriteHeader(http.StatusNotImplemented)
 		}
@@ -49,6 +60,70 @@ func (cc *contractController) getContracts(w http.ResponseWriter, r *http.Reques
 	encodeResponseAsJSON(contracts, w)
 }
 
+func (cc *contractController) getContract(id int, w http.ResponseWriter, r *http.Request) {
+	contract, err := models.GetContractByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(contract, w)
+}
+
 func (cc *contractController) createContractHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("create contract handler"))
+	contract, err := cc.parseContractRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not parse user object"))
+		return
+	}
+	contract, err = models.CreateContract(contract)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(contract, w)
+}
+
+func (cc *contractController) editContractHandler(id int, w http.ResponseWriter, r *http.Request) {
+	contract, err := cc.parseContractRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not parse user object"))
+		return
+	}
+	if id != contract.ID {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("ID of submitted user must match ID in URL"))
+		return
+	}
+
+	contract, err = models.UpdateContract(contract)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(contract, w)
+}
+
+func (cc *contractController) deleteContractHandler(id int, w http.ResponseWriter, r *http.Request) {
+	err := models.DeleteContract(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (uc *contractController) parseContractRequest(r *http.Request) (models.Contract, error) {
+	dec := json.NewDecoder(r.Body)
+	var contract models.Contract
+	err := dec.Decode(&contract)
+	if err != nil {
+		return models.Contract{}, err
+	}
+	return contract, nil
 }
--- server/models/contract.go
+++ server/models/contract.go
@@ -1,6 +1,9 @@
 package models
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type Contract struct {
 	ID         int
@@ -19,6 +22,15 @@ func GetContracts() []*Contract {
 	return contractsCollection
 }
 
+func GetContractByID(id int) (Contract, error) {
+	for _, currContract := range contractsCollection {
+		if currContract.ID == id {
+			return *currContract, nil
+		}
+	}
+	return Contract{}, fmt.Errorf("contract with such id '%v' does not exists", id)
+}
+
 func CreateContract(newContract Contract) (Contract, error) {
 	if newContract.ID != 0 {
 		return Contract{}, errors.New("new user must not include ID field or must be set to 0")
@@ -27,4 +39,24 @@ func CreateContract(newContract Contract) (Contract, error) {
 	nextContractID++
 	contractsCollection = append(contractsCollection, &newContract)
 	return newContract, nil
+}
+
+func UpdateContract(contract Contract) (Contract, error) {
+	for i, currContract := range contractsCollection {
+		if contract.ID == currContract.ID {
+			contractsCollection[i] = &contract
+			return contract, nil
+		}
+	}
+	return Contract{}, fmt.Errorf("contract not found")
+}
+
+func DeleteContract(id int) error {
+	for i, currContract := range contractsCollection {
+		if currContract.ID == id {
+			contractsCollection = append(contractsCollection[:i], contractsCollection[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("user with such ID '%v' does not exists", id)
 }
